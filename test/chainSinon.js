2cbda1ca891973edc18aced909cc298b6214d868
Dane Summers <dsummersl@yahoo.com>
2014-08-28 09:41:27 -0400

    Adds test cases

---
+++ test/chainSinon.js
@@ -0,0 +1,77 @@
+var expect = require("chai").expect;
+var chainSinon = require("../chainSinon");
+
+describe("chainSinon", function() {
+
+	it("stubs one call", function() {
+		var stubs = chainSinon("one");
+		expect(stubs.oneObj).to.be.an("object");
+		expect(stubs.one()).to.be.an("object");
+	});
+
+	it("stubs a string with multiple calls", function() {
+		var stubs = chainSinon("first.second.third");
+		expect(stubs.first()).to.be.an("object");
+		expect(stubs.first().second()).to.be.an("object");
+		expect(stubs.first().second().third()).to.be.an("object");
+		expect(stubs.firstObj).to.be.an("object");
+		expect(stubs["first.secondObj"]).to.be.an("object");
+		expect(stubs["first.second.thirdObj"]).to.be.an("object");
+	});
+
+	it("stubs an array of strings", function() {
+		var stubs = chainSinon(["one.thing", "another"]);
+		expect(stubs.one()).to.be.an("object");
+		expect(stubs.one().thing()).to.be.an("object");
+		expect(stubs.another()).to.be.an("object");
+		expect(stubs["one.thingObj"]).to.be.an("object");
+		expect(stubs["anotherObj"]).to.be.an("object");
+	});
+
+	it('stubs methods of an object', function() {
+		var TestObject = (function() {
+
+			function TestObject() {}
+
+			TestObject.prototype.a = function() {
+				return 'a';
+			};
+
+			TestObject.prototype.b = function() {
+				return 'b';
+			};
+
+			return TestObject;
+
+		})();
+		var obj = new TestObject();
+		var stubs = chainSinon(obj, ["a", "b"]);
+		stubs.a.returns(4);
+		expect(stubs.aObj).to.be.an("object");
+		expect(stubs.bObj).to.be.an("object");
+		expect(obj.a()).to.equal(4);
+	});
+
+	return it('stubs sub-methods of an object', function() {
+		var TestObject = (function() {
+
+			function TestObject() {}
+
+			TestObject.prototype.a = function() {
+				return 'a';
+			};
+
+			TestObject.prototype.b = function() {
+				return 'b';
+			};
+
+			return TestObject;
+
+		})();
+		var obj = new TestObject();
+		var stubs = chainSinon(obj, ["a.b.c", "b"]);
+		stubs['a.b.c'].returns(4);
+		expect(stubs["a.b.cObj"]).to.be.an("object");
+		expect(obj.a().b().c()).to.equal(4);
+	});
+});
